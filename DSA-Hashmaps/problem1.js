8c8e0a7fd6723ed01320e2b040b616652eb7fdd3
Roxanne <roxanne-m@users.noreply.github.com>
2021-02-11 23:51:36 -0800

    first commit

---
+++ DSA-Hashmaps/problem1.js
@@ -0,0 +1,28 @@
+// Problem 1: Create a HashMap class
+
+/*
+Walk through the HashMap implementation in the curriculum 
+and understand it well. Then write a HashMap class and its 
+core functions with open addressing as the collision resolution mechanism.
+
+a.) Export your HashMap module
+
+b.) Create a .js file called HashMaps_drills. In the file import the HashMap module. Create a function called main()
+
+c.) Inside your main() function, create a hash map called lotr.
+
+d.) For your hash map that you have created, set the MAX_LOAD_RATIO = 0.5 and SIZE_RATIO = 3.
+
+e.) Add the following items to your hash map: {"Hobbit": "Bilbo"}, {"Hobbit": "Frodo"},
+{"Wizard": "Gandalf"}, {"Human": "Aragorn"}, {"Elf": "Legolas"}, {"Maiar": "The Necromancer"},
+{"Maiar": "Sauron"}, {"RingBearer": "Gollum"}, {"LadyOfLight": "Galadriel"}, {"HalfElven": "Arwen"},
+{"Ent": "Treebeard"}
+
+f.) Print your hash map and notice the length and items that are hashed in your hash map. Have you hashed all the items you were asked to?
+
+g.) Retrieve the value that is hashed in the key "Maiar" and Hobbit.
+
+h.) What are the values of Maiar and Hobbit that you have? Is there a discrepancy? Explain your answer.
+
+i.) What is the capacity of your hash table after you have hashed all the above items? Explain your answer.
+*/
